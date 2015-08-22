0ffb4b3f220bd9a5b81b62d6ecc04e8cdcb5215e
Hagen Kaye <hagen.kaye@dejero.com>
2015-08-22 18:41:11 -0400

    flash blue led once a second

---
+++ weather-radio.ino
@@ -0,0 +1,23 @@
+int ledOutput = D7;
+
+void setup()
+{
+  pinMode(ledOutput, OUTPUT);
+}
+
+int lastSecond = -1;
+int lastMinute = -1;
+
+void loop()
+{
+  // process tasks once a second
+  if (lastSecond != Time.second()) {
+    lastSecond = Time.second();
+
+    if (lastSecond & 1) {
+      digitalWrite(ledOutput, LOW);
+    } else {
+      digitalWrite(ledOutput, HIGH);
+    }
+  }
+}
